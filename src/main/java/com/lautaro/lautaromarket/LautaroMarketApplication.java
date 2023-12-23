bb82ee49f0611f40a3ac3c51c348a459b6e24db5
Lautaro <lautaroballesteros@gmail.com>
2023-12-23 07:59:06 -0500

    Primer commit

---
+++ src/main/java/com/lautaro/lautaromarket/LautaroMarketApplication.java
@@ -0,0 +1,13 @@
+package com.lautaro.lautaromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LautaroMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LautaroMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lautaro/lautaromarket/LautaroMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautaro.lautaromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LautaroMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
